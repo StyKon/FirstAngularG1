8008af07c9a54fe8b9056e8dcec2de83f7a2ba3e
StyKon <76910363+StyKon@users.noreply.github.com>
2021-02-13 13:48:40 +0100

    Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { C2Component } from './Views/c2/c2.component';
+import { C1Component } from './Views/C2/c1/c1.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    C2Component
+    C2Component,
+    C1Component
   ],
   imports: [
     BrowserModule,
