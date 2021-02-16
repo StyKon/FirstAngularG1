a01d59bcfb992c4d4a880b121ecc86b77c6fe810
StyKon <76910363+StyKon@users.noreply.github.com>
2021-02-16 13:11:05 +0100

    Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { C2Component } from './Views/c2/c2.component';
 import { C1Component } from './Views/C2/c1/c1.component';
+import { MedecinsComponent } from './medecins/medecins.component';
+import { PatientsComponent } from './patients/patients.component';
+import { DiabeteComponent } from './Medecins/diabete/diabete.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     C2Component,
-    C1Component
+    C1Component,
+    MedecinsComponent,
+    PatientsComponent,
+    DiabeteComponent
   ],
   imports: [
     BrowserModule,
